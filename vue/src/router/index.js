2f63b7ededed161d0b5098f6c9e34bd6cd81d05f
kimeu254 <jimkimeu254@gmail.com>
2022-09-15 17:43:24 +0300

    Created an AuthLayout

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,32 +4,53 @@ import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import DefaultLayout from "../components/layouts/DefaultLayout.vue";
 import Surveys from "../views/Surveys.vue";
+import store from "../store";
+import AuthLayout from "../components/layouts/AuthLayout.vue";
 
 const  routes = [
   {
     path: '/',
     redirect: '/home',
     component: DefaultLayout,
+    meta: {requiresAuth: true},
     children: [
       {path: '/home', name: 'Home', component: Home},
       {path: '/surveys', name: 'Surveys', component: Surveys}
     ]
   },
   {
-    path: '/login',
-    name: 'login',
-    component: Login
-  },
-  {
-    path: '/register',
-    name: 'register',
-    component: Register
+    path: '/auth',
+    redirect: '/login',
+    name: 'Auth',
+    component: AuthLayout,
+    children: [
+      {
+        path: '/login',
+        name: 'login',
+        component: Login
+      },
+      {
+        path: '/register',
+        name: 'register',
+        component: Register
+      },
+    ]
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.meta.requiresAuth && !store.state.user.token) {
+    next({name: 'login'})
+  } else if(store.state.user.token && (to.name === 'login' || to.name === 'register')) {
+    next({name: 'home'});
+  } else {
+    next()
+  }
 })
 
 export default router;
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>
-
-  </div>
+  <h3>Register</h3>
+  <hr />
+  <form>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <button type="submit" class="btn btn-primary">Login</button>
+    <hr />
+    <router-link :to="{name: 'login'}">
+      <button type="button" class="btn btn-link">Login</button>
+    </router-link>
+    <button type="button" class="btn btn-link">Reset Password</button>
+  </form>
 </template>
 
 <script>
